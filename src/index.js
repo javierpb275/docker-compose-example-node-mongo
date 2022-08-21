f387405129f3861f8a50c604d5e0a26695e225c7
javierpb275 <javierpb275@gmail.com>
2022-08-21 15:37:40 +0200

    added volume for node, installed nodemon, created new route and added new command in package.json to run server in development mode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const indexRouter = require('./routes/index.routes')
 
 const app = express();
 
 require('./database');
+
+app.use('/', indexRouter)
 
 app.listen(3000);
 console.log('server is up on port', 3000)
